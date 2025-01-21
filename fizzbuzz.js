28b098883e3337e00006e2870ffffcfa9655806d
Emine Oraklabel <emine.oraklabel@softwire.com>
2025-01-21 11:25:56 +0000

    add multiple 7

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -10,9 +10,11 @@ function fizzbuzz() {
         if (i % 3 === 0 && i % 5 === 0) {
             console.log("FizzBuzz");
         } else if (i % 3 === 0) {
-            console.log("Fizz")
+            console.log("Fizz");
         } else if (i % 5 === 0) {
             console.log("Buzz");
+        } else if (i % 7 === 0) {
+            console.log("Bang");
         } else {
             console.log(i);
         }
